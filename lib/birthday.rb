9a96589ca59e509a8fd2a39bfbabaa7ad6d2af6d
Antonio Clavijo Cortes <littletonito@gmail.com>
2020-10-11 21:31:25 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,5 +12,11 @@ def happy_birthday(birthday_kids)
 end
 
 def age_appropriate_birthday(birthday_kids)
-
+  birthday_kids.each do |name, age|
+    if name =< 12
+      puts "Happy Birthday #{name}! You are now #{age} years old!"
+    else
+      puts "You are too old for this."
+    end
+  end
 end
